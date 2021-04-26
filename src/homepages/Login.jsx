d7c6f232e5a9b85b6feab10a6601cd47a1037859
soobinkiki <j.sb.parkk@gmail.com>
2021-04-26 00:16:59 -0700

    testing

--- src/homepages/Login.jsx
+++ src/homepages/Login.jsx
@@ -9,17 +9,17 @@ const SERVER_URL = process.env.REACT_APP_SERVER_URL
 
 const handleGoogleClick = async (e) => {
     console.log('Google Clicked!')
-    window.location.href = `https://puzzle-2048.herokuapp.com/auth/google`
+    window.location.href = `${SERVER_URL}/auth/google`
 }
 
 const handleGithubClick = e => {
     console.log('Github Clicked!')
-    window.location.href=`https://puzzle-2048.herokuapp.com/auth/github`
+    window.location.href=`${SERVER_URL}/auth/github`
 }
 
 const handleFacebookClick = e => {
     console.log('Facebook Clicked!')
-    window.location.href=`/auth/facebook`
+    window.location.href=`${SERVER_URL}/auth/facebook`
 }
 
 const Login = ({ user }) => {
